75f34595581de500788059e8001f9874333c35ea
CXPhoenix <jate9229@gmail.com>
2023-02-12 13:15:32 +0800

    fix: 🐛 調整行距與文字區域寬度

--- src/components/MealIntro.vue
+++ src/components/MealIntro.vue
@@ -1,10 +1,12 @@
 <script setup></script>
 <template>
-  <div class="space-y-4 py-3 px-[10vw]">
-    <p class="p-font indent-9 text-lg">
+  <div
+    class="space-y-4 py-3 px-10 text-xl leading-9 sm:px-[18vw] sm:text-2xl sm:leading-10 lg:space-y-8 lg:text-3xl lg:leading-[3.5rem] xl:space-y-10 xl:px-[22vw]"
+  >
+    <p class="p-font indent-9">
       本校創立於民國42年，歷經省立臺北復興中學、臺北市復興中學、臺北市復興高中，荏苒已屆70週年，70年來栽培無數莘莘學子貢獻社會良多。
     </p>
-    <p class="indent-9 text-lg">
+    <p class="indent-9">
       然本校位處大屯火山群彙山麓，深受硫磺侵蝕，校舍老舊易損亟需翻新加固；又逢新冠肺炎肆虐，除原有之經濟不力學生外，家庭經濟頓失所依者亦眾；各項校務發展計劃亦需自籌經費，故藉70週年校慶機會，擬發起臺北市復興高級中學校務發展基金募款計畫。
     </p>
   </div>
